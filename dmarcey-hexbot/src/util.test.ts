7a2cb9929aac7574b63bc21ea68def2398305218
Derrick Marcey <demarcey@microsoft.com>
2019-06-12 10:38:45 -0400

    Added utils to do color comparison.

--- dmarcey-hexbot/src/util.test.ts
+++ dmarcey-hexbot/src/util.test.ts
@@ -1,4 +1,4 @@
-import { getColorString, hexToColor } from "./util";
+import { getColorString, hexToColor, colorDistance, closestColor, evaluateColors } from "./util";
 
 describe("Utilities", () => {
     describe("getColorString", () => {
@@ -30,6 +30,74 @@ describe("Utilities", () => {
             const expected = { red: 255, green: 255, blue: 255 };
             const actual = hexToColor(color);
             expect(actual).toEqual(expected);
+        });
+    });
+
+    describe("colorDistance", () => {
+        it("distance between same colors is 0", () => {
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 0, green: 0, blue: 0 };
+            const expected = 0.0;
+            const actual = colorDistance(color1, color2);
+            expect(actual).toBeCloseTo(expected);
+        });
+
+        it("distance between black and white is sqrt(255^2+255^2+255^2)", () => {
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = 441.67296;
+            const actual = colorDistance(color1, color2);
+            expect(actual).toBeCloseTo(expected, 5);
+        });
+
+        it("distance between white and black is sqrt(255^2+255^2+255^2)", () => {
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = 441.67296;
+            const actual = colorDistance(color2, color1);
+            expect(actual).toBeCloseTo(expected, 5);
+        });
+    });
+
+    describe("closestColor", () => {
+        it("red is closer to black than white", () => {
+            const targetColor = { red: 255, green: 0, blue: 0 };
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = { color: color1, distance: 255 };
+            const actual = closestColor(targetColor, color1, color2);
+            expect(actual.color).toBe(expected.color);
+            expect(actual.distance).toBeCloseTo(expected.distance);
+        });
+
+        it("purple is closer to white than black", () => {
+            const targetColor = { red: 255, green: 0, blue: 255 };
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = { color: color2, distance: 255 };
+            const actual = closestColor(targetColor, color1, color2);
+            expect(actual.color).toBe(expected.color);
+            expect(actual.distance).toBeCloseTo(expected.distance);
+        });
+    });
+
+    describe("evaluateColors", () => {
+        it("black vs white is not in range of red", () => {
+            const targetColor = { red: 255, green: 0, blue: 0 };
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = undefined;
+            const actual = evaluateColors(targetColor, color1, color2, 254);
+            expect(actual).toBe(expected);
+        });
+
+        it("black vs white - black is in range of dark red", () => {
+            const targetColor = { red: 150, green: 0, blue: 0 };
+            const color1 = { red: 0, green: 0, blue: 0 };
+            const color2 = { red: 255, green: 255, blue: 255 };
+            const expected = color1;
+            const actual = evaluateColors(targetColor, color1, color2, 254);
+            expect(actual).toBe(expected);
         });
     });
 });
--- dmarcey-hexbot/src/util.ts
+++ dmarcey-hexbot/src/util.ts
@@ -11,3 +11,23 @@ export function hexToColor(hex: string): IColor {
         blue: parseInt(hex.substr(5, 2), 16)
     };
 }
+
+export function colorDistance(color1: IColor, color2: IColor) {
+    return Math.sqrt(Math.pow(color1.red - color2.red, 2) + Math.pow(color1.green - color2.green, 2) + Math.pow(color1.blue - color2.blue, 2));
+}
+
+export function closestColor(targetColor: IColor, color1: IColor, color2: IColor): { color: IColor; distance: number } {
+    const color1Distance = colorDistance(targetColor, color1);
+    const color2Distance = colorDistance(targetColor, color2);
+
+    if (color1Distance < color2Distance) {
+        return { color: color1, distance: color1Distance };
+    }
+
+    return { color: color2, distance: color2Distance };
+}
+
+export function evaluateColors(targetColor: IColor, color1: IColor, color2: IColor, range: number): IColor | undefined {
+    const closest = closestColor(targetColor, color1, color2);
+    return closest.distance <= range ? closest.color : undefined;
+}
